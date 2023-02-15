b81c2b916e1245e4adb9d67b314311621ce7b0cd
Juliia228 <miss-komarova2017@yandex.ru>
2023-02-15 03:09:32 +0300

    Base code

---
+++ src/Complex.java
@@ -0,0 +1,13 @@
+public class Complex {
+    double real, imagin;
+
+    public Complex() {
+        this(0.0, 0.0);
+    }
+
+    public Complex(double real, double imagin) {
+        this.real = real;
+        this.imagin = imagin;
+    }
+
+}
